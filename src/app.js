7040134021b07a8cb89281994553e74b44a3fe49
Prerna <omprernaroy@gmail.com>
2025-05-17 22:03:53 +0530

    fix:delete queueService file

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ dotenv.config();
 app.use(express.json());
 app.use('/api', notificationRoutes);
 
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
